87fe60506226278ec83ffc26131d49ec4fe33a00
kimseongsoo <tjdtn1031@naver.com>
2022-12-13 23:09:00 +0900

    모델 적재

--- app/src/main/java/com/example/listencarefree/SoundActivity.java
+++ app/src/main/java/com/example/listencarefree/SoundActivity.java
@@ -1,15 +1,26 @@
 package com.example.listencarefree;
 
-import android.media.Image;
+import static android.content.ContentValues.TAG;
+
+import android.app.Activity;
+import android.content.res.AssetFileDescriptor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import org.tensorflow.lite.Interpreter;
+
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.w3c.dom.Text;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
 
 public class SoundActivity extends AppCompatActivity {
 
@@ -17,6 +28,7 @@ public class SoundActivity extends AppCompatActivity {
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_sound);
+
     }
 
     public void onDefault(View v){
@@ -26,15 +38,22 @@ public class SoundActivity extends AppCompatActivity {
 
         TextView text = findViewById(R.id.textView5);
 
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName()); // 여분의 키
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR"); // 언어 설정
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 image.setImageResource(R.drawable.defaultbutton);
                 text.setText("현재 기본감지가 실행되고 있습니다.");
+
+                Interpreter tflite = getTfliteInterpreter("default_mfcc_model.tflite");
             }
         });
 
     }
+
 
     public void onSiren(View v){
         final ImageButton btn = findViewById(R.id.imageButton2);
@@ -43,11 +62,17 @@ public class SoundActivity extends AppCompatActivity {
 
         TextView text = findViewById(R.id.textView5);
 
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName()); // 여분의 키
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR"); // 언어 설정
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 image.setImageResource(R.drawable.sirenbutton);
                 text.setText("현재 사이렌감지가 실행되고 있습니다.");
+
+                Interpreter tflite = getTfliteInterpreter("default_mfcc_model.tflite");
             }
         });
 
@@ -60,11 +85,17 @@ public class SoundActivity extends AppCompatActivity {
 
         TextView text = findViewById(R.id.textView5);
 
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName()); // 여분의 키
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR"); // 언어 설정
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 image.setImageResource(R.drawable.babybutton);
                 text.setText("현재 울음감지가 실행되고 있습니다");
+
+                Interpreter tflite = getTfliteInterpreter("default_mfcc_model.tflite");
             }
         });
     }
@@ -76,13 +107,38 @@ public class SoundActivity extends AppCompatActivity {
 
         TextView text = findViewById(R.id.textView5);
 
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE,getPackageName()); // 여분의 키
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,"ko-KR"); // 언어 설정
+
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 image.setImageResource(R.drawable.screambutton);
                 text.setText("현재 비명감지가 실행되고 있습니다");
+
+                Interpreter tflite = getTfliteInterpreter("default_mfcc_model.tflite");
             }
         });
 
+    }
+
+    private Interpreter getTfliteInterpreter(String modelPath){
+        try {
+            return new Interpreter(loadModelFile(SoundActivity.this, modelPath));
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private MappedByteBuffer loadModelFile(Activity activity, String modelPath) throws IOException{
+        AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(modelPath);
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
     }
 }
